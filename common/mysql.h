ce1ac4fbde2d3b546936ad07d6a748958f6d2198
Vkontakte <git-source@vk.com>
2014-03-04 05:23:58 +0400

    VK/KittenPHP-DB-Engine commit

--- common/mysql.h
+++ common/mysql.h
@@ -1,18 +1,18 @@
 /*
-    This file is part of VK/KittenDB-Engine.
+    This file is part of VK/KittenPHP-DB-Engine.
 
-    VK/KittenDB-Engine is free software: you can redistribute it and/or modify
+    VK/KittenPHP-DB-Engine is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
 
-    VK/KittenDB-Engine is distributed in the hope that it will be useful,
+    VK/KittenPHP-DB-Engine is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with VK/KittenDB-Engine.  If not, see <http://www.gnu.org/licenses/>.
+    along with VK/KittenPHP-DB-Engine.  If not, see <http://www.gnu.org/licenses/>.
 
     Copyright 2010 Vkontakte Ltd
               2010 Nikolai Durov
--- net/net-http-server.h
+++ net/net-http-server.h
@@ -1,18 +1,18 @@
 /*
-    This file is part of VK/KittenDB-Engine Library.
+    This file is part of VK/KittenPHP-DB-Engine Library.
 
-    VK/KittenDB-Engine Library is free software: you can redistribute it and/or modify
+    VK/KittenPHP-DB-Engine Library is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 2 of the License, or
     (at your option) any later version.
 
-    VK/KittenDB-Engine Library is distributed in the hope that it will be useful,
+    VK/KittenPHP-DB-Engine Library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with VK/KittenDB-Engine Library.  If not, see <http://www.gnu.org/licenses/>.
+    along with VK/KittenPHP-DB-Engine Library.  If not, see <http://www.gnu.org/licenses/>.
 
     Copyright 2010-2012 Vkontakte Ltd
               2010-2012 Nikolai Durov
